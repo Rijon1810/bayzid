ad9db44609529dd75122e73af58f2175c4a2ed28
Rijon <rijon.r@yahoo.com>
2021-06-17 00:57:52 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Form from "./Form";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-reportWebVitals();
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: []
+    };
+
+    this.addPerson = this.addPerson.bind(this);
+    this.deletePerson = this.deletePerson.bind(this);
+  }
+
+  addPerson(name, email) {
+    this.setState(prevState => ({
+      people: [...prevState.people, { name, email }]
+    }));
+  }
+
+
+
+  deletePerson(email) {
+    return () => {
+      this.setState(prevState => ({
+        people: prevState.people.filter(person => person.email !== email)
+      }));
+    };
+  }
+
+  render() {
+  
+    return (
+      <div >
+        <Form addPerson={this.addPerson} />
+        <table style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid'}} >
+          <thead>
+            <tr>
+
+              <th>Name</th>
+              <th>EMAIL</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.people.map((person, index) => {
+              return (
+                <tr key={person.email}>
+    
+                  <td>{person.name}</td>
+                  <td>{person.email}</td>
+                  <td>
+                    <button onClick={this.deletePerson(person.email)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
